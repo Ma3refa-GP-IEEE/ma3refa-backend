b5f00943c2d2b4ed48c2d9bc47412f5775e51b8f
Mohamed Hagag <hagag0446@gmail.com>
2026-08-15 22:18:10 +0300

    Security Edit

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,10 @@ Route::middleware('auth:sanctum')->group(function () {
     // 1. Profile Route
     Route::get('/user/profile', [ProfileController::class, 'show']);
 
-     // Quiz Routes
-     Route::post('/quiz/generate', [QuizController::class, 'generate']);
-     Route::post('/quiz/{id}/finish', [QuizController::class, 'finish']);
-     Route::get('/quiz/{quiz_id}', [QuizController::class, 'show']);
+    // Quiz Routes
+    Route::post('/quiz/generate', [QuizController::class, 'generate'])->middleware('throttle:5,1');
+    Route::post('/quiz/{id}/finish', [QuizController::class, 'finish'])->middleware('throttle:10,1');
+    Route::get('/quiz/{quiz_id}', [QuizController::class, 'show']);
 
     // 2. Categories & Subcategories Routes
     Route::get('/categories', [CategoryController::class, 'index']);
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/subcategories/{subcategory}/quizzes', [SubcategoryController::class, 'quizzes']);
 });
     
-require __DIR__.'/auth.php';   
+require __DIR__.'/auth.php';
